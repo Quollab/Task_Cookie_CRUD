4a1ac940b77ff447741c5809d92f7b473d1d7fe3
Ankur-Agrawal-ece20 <ankur.agrawal.ece20@itbhu.ac.in>
2021-08-31 15:40:42 +0530

    Updated design and removed few bugs!

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 function setCookie(key,value){
-    console.log(key,value)
     document.cookie = key+"="+value;
     listCookies();
 }
@@ -12,12 +11,16 @@ function getCookie(key){
     return "Not found!";
 }
 function delCookie(key){
-    console.log(key)
     document.cookie = key+'=; Path=/Task_Cookie_CRUD; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
     listCookies();
 }
 function listCookies(){
-    if(document.cookie=="") return;
+    if(document.cookie==""){
+        list.innerHTML="";
+        empty.style.display="block";
+        return;
+    }
+    else if(empty) empty.style.display="none";
     let cookiearr=document.cookie.split(';');
     list.innerHTML="<tr><th>KEY</th><th>VALUE</th></tr>";
     for(var i=0;i<cookiearr.length;i++){
@@ -34,6 +37,7 @@ let key2=document.getElementById("key2")
 let key3=document.getElementById("key3")
 let val=document.getElementById("val")
 let list=document.getElementById("list")
+let empty=document.getElementById("empty")
 addbtn.onclick = function(){setCookie(key1.value,val.value)};
 getbtn.onclick = function(){alert("Value for given key is "+getCookie(key2.value))};
 delbtn.onclick = function(){delCookie(key3.value)};
